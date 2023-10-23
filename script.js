b59c6ea39c43cd116e18bb1d5b8585326ad96b2c
mks1313 <139466229+mks1313@users.noreply.github.com>
2023-10-23 18:51:14 +0200

    nueva carp

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get a reference to the button element by its id
+    var button = document.getElementById("button");
+
+    // Add a click event listener to the button
+    button.addEventListener("click", function() {
+        // Display an alert with the specified message when the button is clicked
+        alert("This is an awesome page!");
+    });
+});
